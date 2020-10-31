101ea9fce67dfc01c0bcd24b93894524b4e22dd7
rebeckao <rebecka.oefverholm@gmail.com>
2020-10-31 18:58:12 +0100

    Lesson 7

---
+++ src/codility/Lesson07StacksAndQueues.kt
@@ -0,0 +1,51 @@
+package codility
+
+import java.util.*
+
+class Lesson07StacksAndQueues {
+    fun isProperlyNested(nestedString: String): Int {
+        val openingParentheses = Stack<Char>()
+        for (parenthesis in nestedString) {
+            if (parenthesis == '(' || parenthesis == '[' || parenthesis == '{') {
+                openingParentheses.add(parenthesis)
+            } else {
+                if (openingParentheses.isEmpty()) {
+                    return 0
+                }
+                val matchingOpening = openingParentheses.pop()
+                if (!(parenthesis == ')' && matchingOpening == '(')
+                        && !(parenthesis == ']' && matchingOpening == '[')
+                        && !(parenthesis == '}' && matchingOpening == '{')) {
+                    return 0
+                }
+            }
+        }
+        return if (openingParentheses.isEmpty()) 1 else 0
+    }
+
+    fun numberOfSurvivingFish(sizes: IntArray, directions: IntArray): Int {
+        var survivingFish = 0
+        val fishesSwimingDownstream = Stack<Int>()
+        for (fish in sizes.indices) {
+            val currentSize = sizes[fish]
+            if (directions[fish] == 1) {
+                fishesSwimingDownstream.add(currentSize)
+            } else {
+                var isEaten = false
+                while (!isEaten && !fishesSwimingDownstream.isEmpty()) {
+                    if (fishesSwimingDownstream.peek() < currentSize) {
+                        fishesSwimingDownstream.pop()
+                    } else {
+                        isEaten = true
+                    }
+                }
+                if (!isEaten) {
+                    survivingFish++
+                }
+            }
+        }
+        survivingFish += fishesSwimingDownstream.size
+        return survivingFish
+    }
+
+}
---
+++ src/codility/Lesson07StacksAndQueuesTest.kt
@@ -0,0 +1,40 @@
+package codility
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+internal class Lesson07StacksAndQueuesTest {
+
+    @ParameterizedTest
+    @CsvSource(value = [
+        "{[()()]}; 1",
+        "([)()]; 0",
+        "([)]; 0",
+        "([{}]); 1",
+        "([{}]; 0",
+        "()[]{}; 1",
+        "})][({; 0"
+    ], delimiter = ';')
+    fun isProperlyNested(nestedString : String, expected: Int) {
+        assertEquals(expected, Lesson07StacksAndQueues().isProperlyNested(nestedString))
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = [
+        "4, 3, 2, 1, 5; 0, 1, 0, 0, 0; 2",
+        "4, 3; 0, 1; 2",
+        "4, 3; 1, 0; 1",
+        "0, 1000000000; 1, 0; 1",
+        "0; 1; 1",
+        "1000000000; 0; 1",
+        "3, 5, 2; 1, 0, 0; 2",
+        "3, 5, 2, 1; 1, 0, 0, 1; 3",
+        "1000000000, 999999999; 1, 0; 1",
+    ], delimiter = ';')
+    fun numberOfSurvivingFish(sizes: String, directions: String, expected: Int) {
+        assertEquals(expected, Lesson07StacksAndQueues().numberOfSurvivingFish(Util().toIntArray(sizes), Util().toIntArray(directions)))
+    }
+}
